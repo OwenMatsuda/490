b8f5a0a411483993f2a75e24851b6f3f887633ab
Owen Matsuda <owen.matsuda@gmail.com>
2023-04-04 17:36:03 -0400

    Add login form

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+user = None
+
+@app.route('/')
+def index():
+    if (user != "doctor"):
+        return redirect('/login')
+    return render_template('index.html')
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'doctor' or request.form['password'] != 'doctor':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            global user
+            user = "doctor"
+            return redirect('/')
+    return render_template('login.html', error=error)
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
